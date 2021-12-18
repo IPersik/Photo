51951a435bdb256e06452807789b4d1568c87251
Vera <ipeachyou@yandex.ru>
2021-12-19 00:40:57 +0500

    Заготовка фрагмента

--- app/src/main/java/com/example/photooftheday/MainActivity.kt
+++ app/src/main/java/com/example/photooftheday/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.photooftheday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.photooftheday.view.picture.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState==null){
+            supportFragmentManager.beginTransaction().replace(R.id.container, PictureOfTheDayFragment()).commit()
+        }
     }
 }
--- app/src/main/java/com/example/photooftheday/utils/CustomImageView.kt
+++ app/src/main/java/com/example/photooftheday/utils/CustomImageView.kt
@@ -1,4 +1,4 @@
-package utils
+package com.example.photooftheday.utils
 
 import android.content.Context
 import android.util.AttributeSet
