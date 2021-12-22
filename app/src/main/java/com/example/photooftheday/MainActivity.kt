12b76446230843fa2ecfde62dd154367a0bead66
Vera <ipeachyou@yandex.ru>
2021-12-22 18:56:09 +0500

    смена темы

--- app/src/main/java/com/example/photooftheday/MainActivity.kt
+++ app/src/main/java/com/example/photooftheday/MainActivity.kt
@@ -7,9 +7,22 @@ import com.example.photooftheday.view.picture.PictureOfTheDayFragment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if(savedInstanceState==null){
-            supportFragmentManager.beginTransaction().replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+
+        if (isConnecton()) {
+            setTheme(R.style.ThemeConnected)
+        }else{
+            setTheme(R.style.ThemeDisConnected)
         }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+        setContentView(R.layout.activity_main)
+            //if(savedInstanceState==null){
+            //supportFragmentManager.beginTransaction().replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+        }
+    }
+    private fun isConnecton(): Boolean {
+        return true
     }
 }
