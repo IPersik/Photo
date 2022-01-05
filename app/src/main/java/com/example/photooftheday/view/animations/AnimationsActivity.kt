ca849fa031e91bb9d0285e5242ed18a5b56c31fd
Vera <ipeachyou@yandex.ru>
2022-01-05 14:00:36 +0500

    перемешивание элементов

--- app/src/main/java/com/example/photooftheday/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/example/photooftheday/view/animations/AnimationsActivity.kt
@@ -2,9 +2,9 @@ package com.example.photooftheday.view.animations
 
 import android.os.Bundle
 import android.view.Gravity
-import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.transition.ArcMotion
+import androidx.core.view.ViewCompat
 import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
 import com.example.photooftheday.databinding.ActivityAnimationsBinding
@@ -18,22 +18,26 @@ class AnimationsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val titles: MutableList<String> = ArrayList()
+        for (i in 0..4) {
+            titles.add("Item $i")
+        }
+
         binding.button.setOnClickListener {
-            isDirectionRight = !isDirectionRight
-            val params = binding.button.layoutParams as FrameLayout.LayoutParams
-            params.gravity = if (isDirectionRight) {
-                Gravity.BOTTOM or Gravity.END
-            } else {
-                Gravity.TOP or Gravity.START
+            val cb = ChangeBounds()
+            cb.duration = 2000
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer, cb)
+            titles.shuffle()
+
+            binding.transitionsContainer.removeAllViews()
+            titles.forEach {
+                binding.transitionsContainer.addView(TextView(this).apply {
+                    text = it
+                    ViewCompat.setTransitionName(this, it)
+                    gravity = Gravity.CENTER_HORIZONTAL
+                })
             }
-
-            val transition = ChangeBounds()
-            val path = ArcMotion()
-            transition.setPathMotion(path)
-            transition.duration = 3000
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer, transition)
-
-            binding.button.layoutParams = params
         }
     }
 }
