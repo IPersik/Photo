6d302464822a47bf9121d4afaf69b84d90b694d0
Vera <ipeachyou@yandex.ru>
2022-01-05 13:09:52 +0500

    Explode

--- app/src/main/java/com/example/photooftheday/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/example/photooftheday/view/animations/AnimationsActivity.kt
@@ -1,11 +1,18 @@
 package com.example.photooftheday.view.animations
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 import com.example.photooftheday.databinding.ActivityAnimationsBinding
+
+import com.example.photooftheday.R
 
 class AnimationsActivity : AppCompatActivity() {
 
@@ -16,15 +23,52 @@ class AnimationsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener {
-            isTextViewVisible = !isTextViewVisible
-            var transition = TransitionSet()// исчезновение
-            transition.addTransition(Fade())
-            transition.addTransition(ChangeBounds())
-            //transition.ordering = TransitionSet.ORDERING_SEQUENTIAL // указали последовательное выполнение
-            transition.duration = 5000
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,transition)
-            binding.text.visibility = if(isTextViewVisible) View.VISIBLE else View.GONE
-        }
+        binding.recyclerView.adapter = Adapter()
     }
+
+    inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.activity_animations_recycler_item, parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener {
+                val explodeTransition = Explode()
+                val button = it as Button
+                val fadeTransition = Fade()
+                val setTransition = TransitionSet()
+                explodeTransition.duration = 5000
+                fadeTransition.duration = 5000
+                explodeTransition.excludeTarget(button, true)
+                explodeTransition.epicenterCallback = object : Transition.EpicenterCallback() {
+                    override fun onGetEpicenter(transition: Transition): Rect {
+                        val rect = Rect(
+                            button.x.toInt(),
+                            button.y.toInt(),
+                            button.x.toInt(),
+                            button.y.toInt()
+                        )
+                        //val rect = Rect()
+                        //button.getGlobalVisibleRect(rect) // почти то же самое
+                        return rect
+                    }
+                }
+
+                setTransition.addTransition(fadeTransition)
+                setTransition.addTransition(explodeTransition)
+                TransitionManager.beginDelayedTransition(binding.recyclerView, setTransition)
+                binding.recyclerView.adapter = null
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return 32
+        }
+
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
