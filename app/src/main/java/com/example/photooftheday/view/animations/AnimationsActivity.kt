bfc0787dee31be48577049cb8747b0f90795ba3f
Vera <ipeachyou@yandex.ru>
2022-01-05 13:40:56 +0500

    движение кнопки по траектории

--- app/src/main/java/com/example/photooftheday/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/example/photooftheday/view/animations/AnimationsActivity.kt
@@ -1,55 +1,39 @@
 package com.example.photooftheday.view.animations
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import android.widget.FrameLayout
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.*
-
-import com.example.photooftheday.R
+import androidx.transition.ArcMotion
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
 import com.example.photooftheday.databinding.ActivityAnimationsBinding
 
 class AnimationsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAnimationsBinding
 
-    private var isExpand= false
+    private var isDirectionRight = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.imageView.setOnClickListener {
-            isExpand = !isExpand
-
-            val params = binding.imageView.layoutParams as FrameLayout.LayoutParams
-
-            val transitionSet = TransitionSet()
-            val transitionCB = ChangeBounds()
-            val transitionImage = ChangeImageTransform()
-            transitionCB.duration = 2000
-            transitionImage.duration = 2000
-            transitionSet.addTransition(transitionCB)
-            transitionSet.addTransition(transitionImage)
-            TransitionManager.beginDelayedTransition(binding.container, transitionSet)
-
-            if (isExpand) {
-                binding.imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-                params.height = FrameLayout.LayoutParams.MATCH_PARENT
+        binding.button.setOnClickListener {
+            isDirectionRight = !isDirectionRight
+            val params = binding.button.layoutParams as FrameLayout.LayoutParams
+            params.gravity = if (isDirectionRight) {
+                Gravity.BOTTOM or Gravity.END
             } else {
-                binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
-                params.height = FrameLayout.LayoutParams.WRAP_CONTENT
+                Gravity.TOP or Gravity.START
             }
 
-            binding.imageView.layoutParams = params
+            val transition = ChangeBounds()
+            val path = ArcMotion()
+            transition.setPathMotion(path)
+            transition.duration = 3000
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer, transition)
+
+            binding.button.layoutParams = params
         }
     }
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
