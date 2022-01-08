7dcdbc6cc0e2d0bf70627acb845e1d1df629c6a7
Vera <ipeachyou@yandex.ru>
2022-01-08 09:57:13 +0500

    динамический ConstraintSet

--- app/src/main/java/com/example/photooftheday/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/example/photooftheday/view/animations/AnimationsActivity.kt
@@ -5,11 +5,14 @@ import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.Gravity
+import android.view.animation.AnticipateOvershootInterpolator
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
+import com.example.photooftheday.R
 import com.example.photooftheday.databinding.ActivityAnimationsBinding
 
 private const val duration = 1000L
@@ -25,8 +28,19 @@ class AnimationsActivity : AppCompatActivity() {
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.scrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            binding.header.isSelected = binding.scrollView.canScrollVertically(-1)
+        binding.root.setOnClickListener {
+            val constraintSetStart = ConstraintSet()
+            constraintSetStart.clone(this,R.layout.activity_animations)
+            constraintSetStart.connect(R.id.title,ConstraintSet.END,R.id.backgroundImage,ConstraintSet.END)
+            constraintSetStart.connect(R.id.description,ConstraintSet.TOP,R.id.title,ConstraintSet.BOTTOM)
+            /*val constraintSetEnd = ConstraintSet()
+            constraintSetEnd.clone(this,R.layout.activity_animations_end)*/
+
+            val transition = ChangeBounds()
+            transition.duration = 1000
+            transition.interpolator = AnticipateOvershootInterpolator(1f)
+            TransitionManager.beginDelayedTransition(binding.root,transition)
+            constraintSetStart.applyTo(binding.constraintContainer)
         }
     }
 }
